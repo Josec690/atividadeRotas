0dde7e18948e30acd68609c1770d388cf3144b2e
Josec690 <jose.silva690@fatec.sp.gov.br>
2024-03-01 10:39:16 -0300

    full commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const app = express()
+
+app.listen(8081, function(){
+    console.log("Servidor Ativo!")
+})
+
+app.get("/", function(req, res){
+    console.log("Página Inicial")
+    res.send("Página Inicial")
+})
+
+app.get("/cadastrar", function(req, res){
+    console.log("Página Cadastro")
+    res.send("Página Cadastro")
+})
+
+app.get("/produtos/:item/:quantidade ", function(req, res){
+    console.log("Página Produtos")
+    res.send("Página de produtos - Item: " + req.params.item + " quantidade " + req.params.quantidade)
+})
+
+app.get("/contato", function(req, res){
+    console.log("Página Contato")
+    res.send("Página Contato")
+})
